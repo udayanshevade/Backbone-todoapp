ba84834fcfd1415e26f0a93b9dc45648f78ec3e7
Udayan Shevade <udayan.shevade@gmail.com>
2015-11-22 02:58:58 -0800

    Add test for input priority

--- jasmine/todo-spec.js
+++ jasmine/todo-spec.js
@@ -1,6 +1,18 @@
 $(function() {
   'use strict';
 
+  describe('Input', function() {
+
+    it('can be flagged with priority', function() {
+      var inputPriority = $('#header .priority-btn');
+      // click input priority button
+      inputPriority.click();
+      var priority = inputPriority.parents('#header').hasClass('priority');
+      // expect header to have class priority
+      expect(priority).toBe(true);
+      inputPriority.click();
+    });
+  });
 
 
-})();
+}());
