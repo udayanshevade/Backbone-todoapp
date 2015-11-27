9efb1cbd9388609cbf33110b9b00adacb2b89939
Udayan Shevade <udayan.shevade@gmail.com>
2015-11-27 12:38:45 -0800

    Add priority test

--- jasmine/todo-spec.js
+++ jasmine/todo-spec.js
@@ -1,16 +1,21 @@
 $(function() {
   'use strict';
 
-  describe('Input', function() {
+  describe('Priority', function() {
 
-    it('can be flagged with priority', function() {
-      var inputPriority = $('#header .priority-btn');
+    it('can be added during input', function() {
+      var $inputPriority = $('#header .priority-btn');
       // click input priority button
-      inputPriority.click();
-      var priority = inputPriority.parents('#header').hasClass('priority');
+      $inputPriority.click();
+      var priority = $inputPriority.parents('#header').hasClass('priority-1');
       // expect header to have class priority
       expect(priority).toBe(true);
-      inputPriority.click();
+      for (var i = 0; i < 3; i++) {
+        $inputPriority.click();
+      }
+    });
+
+    it('can be used to filter todos', function() {
     });
   });
 
